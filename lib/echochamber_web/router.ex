fdcd612601038cc16055f68cc6b8bb29b11f05d3
Juho Kontiainen <juho.kontiainen@twoday.com>
2024-02-27 14:41:46 +0200

    Add basic auth

--- lib/echochamber_web/router.ex
+++ lib/echochamber_web/router.ex
@@ -1,6 +1,8 @@
 defmodule EchochamberWeb.Router do
   use EchochamberWeb, :router
 
+  import EchochamberWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,18 +10,11 @@ defmodule EchochamberWeb.Router do
     plug :put_root_layout, html: {EchochamberWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
   end
 
   pipeline :api do
     plug :accepts, ["json"]
-  end
-
-  scope "/", EchochamberWeb do
-    pipe_through :browser
-
-    get "/", PageController, :home
-    get "/hello", HelloController, :index
-    get "/hello/:messenger", HelloController, :show
   end
 
   # Other scopes may use custom stacks.
@@ -41,6 +36,48 @@ defmodule EchochamberWeb.Router do
 
       live_dashboard "/dashboard", metrics: EchochamberWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+
+  ## Authentication routes
+
+  scope "/", EchochamberWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [{EchochamberWeb.UserAuth, :redirect_if_user_is_authenticated}] do
+      live "/users/register", UserRegistrationLive, :new
+      live "/users/log_in", UserLoginLive, :new
+      live "/users/reset_password", UserForgotPasswordLive, :new
+      live "/users/reset_password/:token", UserResetPasswordLive, :edit
+    end
+
+    post "/users/log_in", UserSessionController, :create
+  end
+
+  scope "/", EchochamberWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    get "/", PageController, :home
+    get "/hello", HelloController, :index
+    get "/hello/:messenger", HelloController, :show
+
+    live_session :require_authenticated_user,
+      on_mount: [{EchochamberWeb.UserAuth, :ensure_authenticated}] do
+      live "/users/settings", UserSettingsLive, :edit
+      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+    end
+  end
+
+  scope "/", EchochamberWeb do
+    pipe_through [:browser]
+
+    delete "/users/log_out", UserSessionController, :delete
+
+    live_session :current_user,
+      on_mount: [{EchochamberWeb.UserAuth, :mount_current_user}] do
+      live "/users/confirm/:token", UserConfirmationLive, :edit
+      live "/users/confirm", UserConfirmationInstructionsLive, :new
     end
   end
 end
