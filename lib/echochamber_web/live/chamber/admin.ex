d93ea5bfcad66fed8b7ca9c22663418ab7c1ef26
Juho Kontiainen <juho.kontiainen@twoday.com>
2024-03-17 18:35:56 +0200

    Add favicon, fix admin-view styling

--- lib/echochamber_web/live/chamber/admin.ex
+++ lib/echochamber_web/live/chamber/admin.ex
@@ -27,20 +27,8 @@ defmodule EchochamberWeb.Chamber.AdminLive do
       <div class="text-black text-xs text-center py-2">
         You have: <%= @count %> listeners (including you)
       </div>
-
       <div class="flex justify-between content-center mt-2">
-        <div class="flex items-center gap-2 flex-1" phx-update="ignore" id="admin-player">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            class="w-6 h-6"
-          >
-            <path d="M13.5 4.06c0-1.336-1.616-2.005-2.56-1.06l-4.5 4.5H4.508c-1.141 0-2.318.664-2.66 1.905A9.76 9.76 0 0 0 1.5 12c0 .898.121 1.768.35 2.595.341 1.24 1.518 1.905 2.659 1.905h1.93l4.5 4.5c.945.945 2.561.276 2.561-1.06V4.06ZM18.584 5.106a.75.75 0 0 1 1.06 0c3.808 3.807 3.808 9.98 0 13.788a.75.75 0 0 1-1.06-1.06 8.25 8.25 0 0 0 0-11.668.75.75 0 0 1 0-1.06Z" />
-            <path d="M15.932 7.757a.75.75 0 0 1 1.061 0 6 6 0 0 1 0 8.486.75.75 0 0 1-1.06-1.061 4.5 4.5 0 0 0 0-6.364.75.75 0 0 1 0-1.06Z" />
-          </svg>
-          <input type="range" id="lobby_volume" name="volume" min="0" max="100" start="50" />
-        </div>
+        <div class="flex-1"></div>
         <%= if @radio_status.playing? do %>
           <button phx-click="js_pause">
             <svg
@@ -75,7 +63,18 @@ defmodule EchochamberWeb.Chamber.AdminLive do
             </svg>
           </button>
         <% end %>
-        <div class="flex-1"></div>
+        <div class="flex items-center gap-2 flex-1 justify-end" phx-update="ignore" id="admin-player">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            class="w-6 h-6"
+          >
+            <path d="M13.5 4.06c0-1.336-1.616-2.005-2.56-1.06l-4.5 4.5H4.508c-1.141 0-2.318.664-2.66 1.905A9.76 9.76 0 0 0 1.5 12c0 .898.121 1.768.35 2.595.341 1.24 1.518 1.905 2.659 1.905h1.93l4.5 4.5c.945.945 2.561.276 2.561-1.06V4.06ZM18.584 5.106a.75.75 0 0 1 1.06 0c3.808 3.807 3.808 9.98 0 13.788a.75.75 0 0 1-1.06-1.06 8.25 8.25 0 0 0 0-11.668.75.75 0 0 1 0-1.06Z" />
+            <path d="M15.932 7.757a.75.75 0 0 1 1.061 0 6 6 0 0 1 0 8.486.75.75 0 0 1-1.06-1.061 4.5 4.5 0 0 0 0-6.364.75.75 0 0 1 0-1.06Z" />
+          </svg>
+          <input type="range" id="lobby_volume" name="volume" min="0" max="100" start="50" />
+        </div>
       </div>
       <div class="flex justify-between mt-4 flex-wrap grow min-h-0">
         <div class="flex flex-col basis-[48%] gap-4 h-full">
